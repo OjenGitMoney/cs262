809375415aa95910b6cd613727a1d968b76396b2
OjenGitMoney <ojen.goshtasb@gmail.com>
2017-11-03 00:04:06 -0700

    just starting

---
+++ knapSackTest.py
@@ -0,0 +1,25 @@
+
+
+def main():
+	maxWeight = 20
+	weights  = [1, 6, 2, 4, 10]	
+	values = [2, 5, 10, 4, 3]
+	A = [0, 0, 0, 0, 0]
+	chosenValue = 0
+	tempValue = 0
+	
+	for i in range(2**len(weights)):
+		
+		for j in range(len(weights)):
+			tempValue = values[j]
+			
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
